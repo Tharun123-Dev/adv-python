1c999d0339a784e05d038af6cb5127fdb2c4aa55
Tharun123-Dev <anjali.tharun9949@gmail.com>
2025-10-03 12:25:01 +0530

    new

--- day12/chustomize2.py
+++ day12/chustomize2.py
@@ -109,15 +109,36 @@ import re
 #     print("Invalid Phone Number")
 
 
-#pincode checking validate
-regex=r"^[1-9]{1}[0-9]{5}$"
-op=re.search(regex,"123564")
+# #pincode checking validate
+# regex=r"^[1-9]{1}[0-9]{5}$"
+# op=re.search(regex,"123564")
+# if op:
+#     print("valid")
+# else:
+#     print("invalid")
+
+# #valid date like [2025-02-12]
+# regex=r"^[0-9]{4}(-)[0-1]{1}(-)[0-9]{2}$"
+# op=re.search(regex,"2025-12-25")
+# if op:
+#     print("valid")
+# else:
+#     print("invalid")
+
+
+
+
+regex = r'^(19|20)\d{2}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$'
+date =input("eneter a date: ")
+op=re.search(regex,date)
 if op:
     print("valid")
 else:
     print("invalid")
 
-#
+
+
+
 
 
 
